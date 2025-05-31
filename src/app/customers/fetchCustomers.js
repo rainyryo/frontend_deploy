aafc953754f95a831ba896e81f487e6135b4ffac
rainyryo <ryo.t.sust@gmail.com>
2025-05-31 21:42:44 +0900

    まず環境変数の値をログに出すコードを挿入

--- src/app/customers/fetchCustomers.js
+++ src/app/customers/fetchCustomers.js
@@ -1,4 +1,11 @@
 export default async function fetchCustomers() {
+// ココから環境変数の辺りをログに出す
+ console.log(">>> NEXT_PUBLIC_API_ENDPOINT (server) =", process.env.NEXT_PUBLIC_API_ENDPOINT);
+
+const fullUrl = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/allcustomers`;
+console.log(">>> fetch URL =", fullUrl);
+//ここまで
+
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_ENDPOINT}/allcustomers`,
     {
