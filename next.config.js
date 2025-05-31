e11c2dc1723465ab9291648912c56cf123d751df
rainyryo <ryo.t.sust@gmail.com>
2025-05-31 16:06:17 +0900

    output: 'standalone' を追加

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-require('dotenv').config()
+// Next.jsの設定ファイル
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        // Reference a variable that was defined in the .env file and make it available at Build Time
-        API_ENDPOINT: process.env.API_ENDPOINT,
-      },
-}
+    output: 'standalone'
+      }
 
 module.exports = nextConfig
