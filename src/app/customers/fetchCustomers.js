d1ca8efdc4f90876c124cc03c4b9499365d4d557
rainyryo <ryo.t.sust@gmail.com>
2025-05-31 20:47:20 +0900

    `${process.env.NEXT_PUBLIC_API_ENDPOINT}/customers`

--- src/app/customers/fetchCustomers.js
+++ src/app/customers/fetchCustomers.js
@@ -1,6 +1,6 @@
 export default async function fetchCustomers() {
   const res = await fetch(
-    process.env.NEXT_PUBLIC_API_ENDPOINT + "/allcustomers",
+    `${process.env.NEXT_PUBLIC_API_ENDPOINT}/allcustomers`,
     {
       cache: "no-cache",
     }
